51893929bcda7c6a0045030936cbe58614c224ff
tudorpaun76 <tudor.paun76@gmail.com>
2022-04-19 19:21:28 +0100

    Team project app

--- app/src/main/java/com/example/gorillashake/SettingsActivity.java
+++ app/src/main/java/com/example/gorillashake/SettingsActivity.java
@@ -3,9 +3,12 @@ package com.example.gorillashake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,10 +16,14 @@ public class SettingsActivity extends AppCompatActivity {
 
     Button RETURNSETTINGS;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
 
         Button btn = (Button)findViewById(R.id.RETURNSETTINGS);
 
@@ -27,5 +34,13 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
+
+
+
+
 }
+
+
--- app/src/main/java/com/example/gorillashake/ShareActivity.java
+++ app/src/main/java/com/example/gorillashake/ShareActivity.java
@@ -3,20 +3,29 @@ package com.example.gorillashake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ShareActivity extends AppCompatActivity {
 
     Button RETURNSHARE;
+    ImageView insta;
+    ImageView facebook;
+    ImageView twitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
 
-        Button btn = (Button)findViewById(R.id.RETURNSHARE);
+        insta = findViewById(R.id.instaicon);
+        facebook = findViewById(R.id.fbicon);
+        twitter = findViewById(R.id.twiicon);
+
+        Button btn = (Button) findViewById(R.id.RETURNSHARE);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +34,32 @@ public class ShareActivity extends AppCompatActivity {
 
             }
         });
+
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.instagram.com/gorillaz/?hl=en");
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.facebook.com/Gorillaz");
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://twitter.com/gorillaz");
+            }
+        });
+
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
 }
